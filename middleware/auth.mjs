b7584869f55222574c8d34f3aee5ecd77cdae702
 Artem Kedrov <artemkedrov1991@gmail.com>
2024-08-15 15:28:22 +0300

    bug fix

--- middleware/auth.mjs
+++ middleware/auth.mjs
@@ -8,6 +8,7 @@ export const isAuthenticated = async (req, res, next) => {
         const token = req.headers.authorization.split(' ')[1]
         const decodeToken = jwt.verify(token, process.env.JWT_SECRET)
         req.userId = decodedToken.userId
+        next()
     } catch (error) {
         res.status(401).json({ error: 'Unauthorized' })
     }
