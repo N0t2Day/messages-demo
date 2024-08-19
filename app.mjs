670f77e5e4319c0934fc577dc9a1771fe2ba2703
 Artem Kedrov <artemkedrov1991@gmail.com>
2024-08-19 18:15:14 +0300

    updated core file

--- app.mjs
+++ app.mjs
@@ -1,35 +1,67 @@
 import express from 'express'
-import * as mongodb from 'mongodb'
 import session from 'express-session'
 import mongoose from 'mongoose'
 import connectMongoDBSession from 'connect-mongodb-session'
 import dotenv from 'dotenv'
+import authRouter from './routers/auth.mjs'
+import morgan from 'morgan'
+import passport from 'passport'
+import userRouter from './routers/user.mjs'
+import messageRouter from './routers/message.mjs'
+import './middleware/auth.mjs'
+import cors from 'cors'
+import Multer from 'multer'
+import * as path from 'path'
+import { Storage } from '@google-cloud/storage'
 dotenv.config()
-const MongoDBStore = connectMongoDBSession(session)
+// const MongoDBStore = connectMongoDBSession(session)
 
 const MONGODB_URI = process.env.MONGODB_URI
 const PORT = process.env.SERVER_PORT
-console.log(MONGODB_URI)
 const app = express()
-
-const store = new MongoDBStore({
-    uri: MONGODB_URI,
-    collection: 'sessions',
+const multer = Multer({
+    storage: Multer.memoryStorage(),
+})
+const storage = new Storage({
+    projectId: '',
+    keyFilename: '',
 })
 
+app.set('trust proxy', 1)
+
+// const store = new MongoDBStore({
+//     uri: MONGODB_URI,
+//     collection: 'sessions',
+// })
+app.use(cors())
+// logs info about request
+app.use(morgan('common'))
+// converts JSON to JS Object in POST, PUT, PATCH requests
+app.use(express.json())
+// convets form data to JS Object in POST, PUT, PATCH requests
+app.use(express.urlencoded({ extended: true }))
 app.use(
     session({
         secret: process.env.SESSION_SECRET,
         resave: false,
         saveUninitialized: false,
-        store: store,
+        // store: store,
     })
 )
 
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(multer().single('fileData'))
+app.use(authRouter)
+app.use(userRouter)
+app.use(messageRouter)
+
 mongoose
-    .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .connect(MONGODB_URI)
     .then((result) => {
-        app.listen(PORT)
+        app.listen(PORT, () => {
+            console.log('Server launched')
+        })
     })
     .catch((err) => {
         console.log(err)
