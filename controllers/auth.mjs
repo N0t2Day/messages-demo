d4d6644a0f11d056c87a649d5f7d1ddf1f3269d8
 Artem Kedrov <artemkedrov1991@gmail.com>
2024-08-19 18:15:35 +0300

    auth algorithm update

--- controllers/auth.mjs
+++ controllers/auth.mjs
@@ -1,9 +1,11 @@
-const crypto = require('crypto')
-const bcrypt = require('bcryptjs')
-
+import crypto from 'crypto'
+import bcrypt from 'bcryptjs'
+import jsonwebtoken from 'jsonwebtoken'
+import { config } from 'dotenv'
 import User from '../models/user.mjs'
+config()
 
-export const postSignup = async (req, res, next) => {
+const postSignup = async (req, res, next) => {
     try {
         const { userName, email, password } = req.body
         const existingUser = await User.findOne({ email })
@@ -16,33 +18,114 @@ export const postSignup = async (req, res, next) => {
         const newUser = new User({
             name: userName,
             email,
-            hashedPassword,
+            password: hashedPassword,
         })
         await newUser.save()
         res.status(201).json({ message: 'User registered successfully' })
     } catch (error) {
+        console.log(error)
         res.status(500).json({
             error: 'An error occurred while registering the user',
         })
     }
 }
 
-export const postLogin = async (req, res, next) => {
+const postLogin = async (req, res, next) => {
     try {
         const { email, password } = req.body
         const user = await User.findOne({ email })
         if (!user) {
             return res.status(401).json({ error: 'Invalid email or password' })
         }
-        const isPasswordValid = await user.comparePassword(password)
+        const isPasswordValid = await bcrypt.compare(password, user.password)
         if (!isPasswordValid) {
             return res.status(401).json({ error: 'Invalid email or password' })
         }
-        const token = jwt.sign({ userId: user._id }, 'secretKey')
+        const token = jsonwebtoken.sign(
+            { userId: user.id },
+            process.env.JWT_SECRET,
+            { expiresIn: '1d' }
+        )
         req.session.isLoggedIn = true
         req.session.user = user
-        res.status(200).json({ token })
+        req.session.save(() => {
+            res.status(200).json({ token })
+        })
     } catch (error) {
-        res.status(500).json({ error: 'An error occurred while fetching user' })
+        console.log(error)
+        res.status(500).json({ error: 'Unknown error' })
     }
+}
+
+const postForgotPassword = async (req, res, next) => {
+    try {
+        const { email } = req.body
+
+        // Check if the user exists
+        const user = await User.findOne({ email })
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' })
+        }
+
+        // Generate a reset token
+        const resetToken = crypto.randomBytes(20).toString('hex')
+        user.resetToken = resetToken
+        user.resetTokenExpiration = Date.now() + 3600000 // Token expires in 1 hour
+        await user.save()
+        res.status(200).json({ message: 'Password reset token sent' })
+    } catch (error) {
+        console.error('Error generating reset token:', error)
+        res.status(500).json({
+            error: 'An error occurred while generating the reset token',
+        })
+    }
+}
+
+const postResetPassword = async (req, res, next) => {
+    try {
+        const { resetToken, newPassword } = req.body
+
+        // Find the user with the provided reset token
+        const user = await User.findOne({
+            resetToken,
+            resetTokenExpiration: { $gt: Date.now() },
+        })
+        if (!user) {
+            return res
+                .status(401)
+                .json({ error: 'Invalid or expired reset token' })
+        }
+
+        // Encrypt and hash the new password
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(newPassword, salt)
+
+        // Update the user's password and reset token fields
+        user.password = hashedPassword
+        user.resetToken = undefined
+        user.resetTokenExpiration = undefined
+        await user.save()
+
+        res.status(200).json({ message: 'Password reset successful' })
+    } catch (error) {
+        console.error('Error resetting password:', error)
+        res.status(500).json({
+            error: 'An error occurred while resetting the password',
+        })
+    }
+}
+
+const postLogout = (req, res, next) => {
+    req.session.destroy((err) => {
+        console.log(err)
+        res.redirect('/')
+    })
+}
+
+export {
+    postSignup,
+    postLogin,
+    postForgotPassword,
+    postResetPassword,
+    postLogout,
 }
--- routers/auth.mjs
+++ routers/auth.mjs
@@ -1,9 +1,11 @@
 import express from 'express'
 import { check, body } from 'express-validator'
-import authController from '../controllers/auth.mjs'
-import User from '../models/user.mjs'
+import * as authController from '../controllers/auth.mjs'
 
 const router = express.Router()
 
-router.post('/signup', await authController.postSignup)
-router.post('/login', await authController.postLogin)
+router.post('/signup', authController.postSignup)
+router.post('/login', authController.postLogin)
+router.post('/logout', authController.postLogout)
+
+export default router
